73504ab190fb540c631a591682e447828a9dd262
vijaykamal <vijaykamal_ramasamy@neimanmarcus.com>
2023-05-30 23:03:27 +0530

    Complete website

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,6 +3,7 @@ import { technologies } from '../constants';
 import { SectionWrapper } from '../hoc';
 const Tech = () => {
   return (
+    <>
     <div className='flex flex-row flex-wrap justify-center gap-10'>{
       technologies.map((technology)=>(
         <div className='w-28, h-28' key={technology}>
@@ -10,6 +11,7 @@ const Tech = () => {
         </div>
       ))
     }</div>
+    </>
   )
 }
 
