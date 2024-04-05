981ab0e71c392dad7697dacd998866616a40166d
vijaykamalr <vijaykamalr@gmail.com>
2024-04-05 09:16:35 +0000

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ function App() {
       <BrowserRouter>
         <div className="relative z-0 bg-primary">
           <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-            commit2
             <Navbar />
             <Hero />
           </div>
@@ -28,6 +27,7 @@ function App() {
           <Feedbacks />
           <div className="relative z-0">
             <Contact />
+            commit 3
             <StarsCanvas />
           </div>
         </div>
